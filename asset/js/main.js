db34c470a4465ff958ece1f520aa79499ba01e17
Federico Airoldi <airoldibusiness@gmail.com>
2023-03-22 23:18:54 +0100

    FASE 5 - calcolo il prezzo in base ai kilometri

--- asset/js/main.js
+++ asset/js/main.js
@@ -12,6 +12,8 @@ function getUserInput() {
     let age = document.getElementById('age').value;
     const prckm = 0.21;
 
+    document.getElementById('priceresult').innerHTML = prckm * km ;
+
     console.log(km, age)
 
 }
