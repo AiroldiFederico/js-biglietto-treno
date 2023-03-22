cc11768d6f223720dc26e52c069d02e10a0d357a
Federico Airoldi <airoldibusiness@gmail.com>
2023-03-22 23:42:22 +0100

    FASE 7 - arrotondamento a due decimali

--- asset/js/main.js
+++ asset/js/main.js
@@ -12,12 +12,12 @@ function getUserInput() {
     let age = document.getElementById('age').value;
     const prckm = 0.21;
 
-    document.getElementById('priceresult').innerHTML = prckm * km ;
+    document.getElementById('priceresult').innerHTML = (prckm * km).toFixed(2);
 
     if (age < 18) {
-        document.getElementById('priceresult').innerHTML = (prckm * km) * 0.8;
+        document.getElementById('priceresult').innerHTML = ((prckm * km) * 0.8).toFixed(2);
     } else if (age > 65) {
-        document.getElementById('priceresult').innerHTML = (prckm * km) * 0.6;
+        document.getElementById('priceresult').innerHTML = ((prckm * km) * 0.6).toFixed(2);
     }
 
 }
